1716d13e7f0918792136b2056bb2fb5f1228cdc6
Jean-Christophe Jameux <echologie@echos.systems>
2018-11-16 20:23:10 +0100

    Meilleur design

--- i.js
+++ i.js
@@ -1,13 +1,12 @@
 let i=X(`Bonjour.
 Bonjour.
-Je suis tombé par hasard sur votre site et je me demandais ce qu'on pouvait y trouver.
-Des informations et des documents à destination de mes élèves, mais évidement libre à chacun d'y prendre ce qu'il souhaite.
+Je suis tombé par hasard sur votre site et je suis curieux de que l'on peut y trouver.
+On y trouve des conseils et des documents à destination de mes élèves.
 Et vous êtes ?
 Professeur de mathématiques.
 Ah ! Je vois. Encore un de ses blogs où un enseignant raconte le cosinus à sa façon.
-À vrai dire, il y a assez peu de mathématiques sur ce blog, si tant est que ce terme soit adapté à la structure du site. Je parle plutôt des principes qui sous-tendent les mécanismes de l'apprentissage.
-Et de quel genre de principes parle-t-on, au juste ?
-Eh bien, Vous pouvez lire <a title='Dialogues pédagogiques' href='DialoguesPedagogiques/0.html'>mes articles</a>. J'espère qu'ils vous éclaireront. Les principes en question sont parfois très anciens, parfois très modernes, mais ils sont malheureusement toujours mal connus, alors qu'ils sont susceptibles d'apporter une aide précieuse dans une multitude de situations d'apprentissage, ou même de la vie courante.
-Je vois...
-Si vous souhaitez être tenu au courant de la publication des articles à venir, vous trouverez des liens vers les réseaux sociaux où je publie les sorties des articles, ainsi que des boutons de partage sur chaque page.
-Dans ce cas, je ne me priverai pas d'y faire un tour.`);c("i",i)
+À vrai dire, il y a assez peu de mathématiques sur ce site. On y discute plutôt des mécanismes de l'apprentissage.
+Et de quel genre de mécanismes parle-t-on, au juste ?
+Eh bien, Vous pouvez lire mes Dialogues Pédagogiques, accessibles en cliquant sur l'icône en bas à droite. J'espère qu'ils vous éclaireront. Les sujets que j'y aborde sont parfois très anciens, parfois très modernes, mais ils sont malheureusement toujours mal connus, quand ils ne sont pas en plus entachés d'idées-reçues absurdes, alors qu'ils sont susceptibles d'apporter une aide précieuse dans une multitude de situations d'apprentissage, ou même de la vie courante.
+Je vois... Dans ce cas, le mieux c'est que je me fasse mon idée en allant voir par moi même.
+Je vous en prie, et si vous avez la moindre question, n'hésitez pas à me contacter sur le réseau social de votre choix.`);c("i",i)
